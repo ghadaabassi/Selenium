3d8709b7076e584e6137a3f8788666ae40d16465
ghada <gh.abassi01@gmail.com>
2024-11-23 20:14:11 +0100

    addSpringTestContext

--- src/test/java/org/example/selenium/SeleniumApplicationTests.java
+++ src/test/java/org/example/selenium/SeleniumApplicationTests.java
@@ -4,10 +4,12 @@ package org.example.selenium;
 import org.junit.runner.RunWith;
 import io.cucumber.junit.Cucumber;
 import io.cucumber.junit.CucumberOptions;
+import org.springframework.boot.test.context.SpringBootTest;
 
 @RunWith(Cucumber.class)
+@SpringBootTest
 @CucumberOptions(
-		features = "src/test/resources/googlesearch.feature",
+		features = "src/test/resources",
 		glue = {"stepDefinitions"},
 		plugin = {"pretty","json:target/cucumber.json"}
 )
