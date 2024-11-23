5a93e3af47c61cf8f58ea85c14356b9981355fd6
ghada <gh.abassi01@gmail.com>
2024-11-23 17:47:52 +0100

    updateApplicationTests

--- src/test/java/org/example/selenium/SeleniumApplicationTests.java
+++ src/test/java/org/example/selenium/SeleniumApplicationTests.java
@@ -7,11 +7,10 @@ import io.cucumber.junit.CucumberOptions;
 
 @RunWith(Cucumber.class)
 @CucumberOptions(
-		features = "src/test/resources",  // Path to the feature files
-		glue = "stepDefinitions",        // Package where step definitions are located
-		plugin = {"json:target/cucumber.json", "pretty"}  // Plugins for reporting
+		features = "src/test/resources",
+		glue = {},
+		plugin = {"json:target/cucumber.json", "pretty"}
 )
 public class SeleniumApplicationTests {
-	// This class is used to run the tests, and it's empty because the annotations handle the configuration.
 }
 
