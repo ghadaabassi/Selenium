24f54b733c981a76688d4c5ce34310bf3e8516c0
ghada <gh.abassi01@gmail.com>
2024-11-23 18:15:47 +0100

    fixPlugin

--- src/test/java/org/example/selenium/SeleniumApplicationTests.java
+++ src/test/java/org/example/selenium/SeleniumApplicationTests.java
@@ -8,7 +8,7 @@ import io.cucumber.junit.CucumberOptions;
 @RunWith(Cucumber.class)
 @CucumberOptions(
 		features = "src/test/resources",
-		glue = {"stepDefinitions"},
+		glue = {"pretty","stepDefinitions"},
 		plugin = {"json:target/cucumber.json"}
 )
 public class SeleniumApplicationTests {
